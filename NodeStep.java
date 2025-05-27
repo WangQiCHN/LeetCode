b6da53fe73eef7f73db1151cb8ca873af01bf31d
王骐 <wangqichn@gmail.com>
2025-05-27 20:28:59 +0800

    dfs, from the last node and calculate the n - 1 and m - 1

---
+++ NodeStep.java
@@ -0,0 +1,9 @@
+public class NodeStep {
+    public int current;
+    public int least;
+
+    public NodeStep(int current, int least) {
+        this.current = current;
+        this.least = least;
+    }
+}
