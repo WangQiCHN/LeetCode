3f5e353e8f0a89e6ae1de8cfed2d70e1a76a6cac
王骐 <wangqichn@gmail.com>
2025-06-02 17:25:26 +0800

    finish 34

--- second/SearchRange.java
+++ second/SearchRange.java
@@ -6,31 +6,10 @@ public class SearchRange {
         System.out.println(result[0] + "," + result[1]);
     }
     public int[] searchRange(int[] nums, int target) {
-        int index = searchSelf(nums, target);
-        if (index == -1) {
-            return new int[] {-1, -1};
-        }
         int left = searchLeft(nums, target);
         int right = searchRight(nums, target);
 
         return new int[] {left, right};
-    }
-
-    private int searchSelf(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            int n = nums[mid];
-            if (n == target) {
-                return mid;
-            } else if (n > target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-
-        return -1;
     }
 
     private int searchLeft(int[] nums, int target) {
@@ -47,6 +26,7 @@ public class SearchRange {
             }
         }
 
+        if (left == nums.length || nums[left] != target) return -1;
         return left;
     }
 
@@ -64,6 +44,7 @@ public class SearchRange {
             }
         }
 
+        if (left - 1 < 0 || nums[left - 1] != target) return -1;
         return left - 1;
     }
 }
