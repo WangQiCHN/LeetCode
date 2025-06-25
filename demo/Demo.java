dce97f6ddc1bb2f822e97d05f8f257f180baf21f
王骐 <wangqichn@gmail.com>
2025-06-25 18:49:24 +0800

    refine last 5 tickets

--- demo/Demo.java
+++ demo/Demo.java
@@ -5,28 +5,36 @@ import java.util.Arrays;
 public class Demo {
 
     public static void main() {
-        Demo d = new Demo();
-        int[] coins = {1, 2, 5};
-        int amount = 11;
-        // int[] nums1 = {1, 2};
-        int result = d.coinChange(coins, amount);
-        System.out.println(result);
+        int[] w = {1,3};
+        Demo d = new Demo(w);
+        for (int i = 0; i < 4; i++) {
+            System.out.println(d.pickIndex(i));
+        }
     }
 
 
-    public int coinChange(int[] coins, int amount) {
-        int[] dps = new int[amount + 1];
-        Arrays.fill(dps, amount + 1);
-        dps[0] = 0;
-        for (int i = 0; i < dps.length; i++) {
-            for (int c : coins) {
-                if (i - c < 0) {
-                    continue;
-                }
-                dps[i] = Math.min(dps[i], 1 + dps[i - c]);
+    int[] p;
+    public Demo(int[] w) {
+        p = new int[w.length + 1];
+        p[0] = 0;
+        for (int i = 1; i < w.length + 1; i++) {
+            p[i] = p[i - 1] + w[i - 1];
+        }
+    }
+    
+    public int pickIndex(int i) {
+        int sum = p[p.length - 1];
+        int index = i;// (int)(Math.random() * sum);
+        int left = 0, right = p.length - 1;
+        while (left < right) {
+            int mid = (right - left) / 2 + left;
+            if (p[mid] > index) {
+                right = mid;
+            } else {
+                left = mid + 1;
             }
         }
 
-        return (dps[amount] == amount + 1) ? -1 : dps[amount];
+        return left - 1;
     }
 }
