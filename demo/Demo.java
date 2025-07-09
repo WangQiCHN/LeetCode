097f911d941be003e3db499ec9107dc8bbfc4821
王骐 <wangqichn@gmail.com>
2025-07-09 21:29:43 +0800

    finish quick sort

--- demo/Demo.java
+++ demo/Demo.java
@@ -3,33 +3,26 @@ package demo;
 class Demo {
     public static void main(String[] args) {
         Demo demo = new Demo();
-        int k = 2;
-        int n = 6;
-        int result = demo.superEggDrop(k, n);
+        String k = "bbbab";
+        int result = demo.longestPalindromeSubseq(k);
         System.out.println(result); // Output: 2
     }
 
-    public int superEggDrop(int k, int n) {
-        int[][] dps = new int[k + 1][n + 1];
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        char[] cs = s.toCharArray();
+        int[] dps = new int[n];
 
-        for (int i = 1; i <= k; i++) {
-            dps[i][1] = 1; // 1 floor requires 1 attempt
-        }
-        for (int j = 1; j <= n; j++) {
-            dps[1][j] = j; // 1 egg requires j attempts for
-        }
-
-        for (int i = 2; i <= k; i++) {
-            for (int j = 2; j <= n; j++) {
-                for (int x = 1; x <= j; x++) {
-                    int temp = 1 + Math.max(dps[i - 1][x - 1], dps[i][j - x]);
-                    if (dps[i][j] == 0 || temp < dps[i][j]) {
-                        dps[i][j] = temp;
-                    }
-                }
+        for (int i = n - 1; i >=0; i--) {
+            dps[i] = 1;
+            int prev = 0;
+            for (int j = i + 1; j < n; j++) {
+                int temp = dps[j];
+                dps[j] = (cs[i] == cs[j] ? (prev + 2) : Math.max(dps[j], dps[j - 1]));
+                prev = temp;
             }
         }
 
-        return dps[k][n];
+        return dps[n - 1];
     }
 }
