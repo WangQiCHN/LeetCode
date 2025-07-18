9527f68152ed7684e91ccec99cd4d7fde6327e5b
王骐 <wangqichn@gmail.com>
2025-07-18 11:57:43 +0800

    finish 4

--- demo/Demo.java
+++ demo/Demo.java
@@ -1,55 +1,35 @@
 package demo;
 
+import java.util.LinkedList;
+
 class Demo {
     public static void main(String[] args) {
+        String s = "/home/user/Documents//../Pictures/";
         Demo demo = new Demo();
-        String answer = demo.getPermutation(3, 3);
-        System.out.println(answer);
+        String result = demo.simplifyPath(s);
+        System.out.println(result);  // Output: /home/user/Pictures
     }
 
-    private boolean[] visited;
-    private StringBuilder sb = new StringBuilder();
-    public String getPermutation(int n, int k) {
-        if (n == 1) return "1";
-        visited = new boolean[n + 1];
-        getIndex(n, n, k - 1);
+    public String simplifyPath(String path) {
+        String[] strs = path.split("/");
+        LinkedList<String> q = new LinkedList<>();
+
+        for (String s : strs) {
+            if (s.equals("..")) {
+                q.removeLast();
+            } else {
+                q.addLast(s);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!q.isEmpty()) {
+            String v = q.pollFirst();
+            sb.append("/" + v);
+        }
 
         return sb.toString();
-    }
 
-    private void getIndex(int total, int n, int k) {
-        if (n == 1) {
-            for (int i = 1; i <= total; i++) {
-                if (visited[i]) continue;
-                else {
-                    sb.append(i);
-                    visited[i] = true;
-                }
-            }
-            return;
-        }
-        int f = factorial(n - 1);
-        int r = k / f;
-        int t = r * f;
-        for (int i = 1; i <= total; i++) {
-            if (visited[i]) continue;
-            else {
-                if (r == 0) {
-                    sb.append(i);
-                    visited[i] = true;
-                    getIndex(total, n - 1, k - t);
-                } else {
-                    r--;
-                }
-            }
-        }
-    }
 
-    private int factorial(int n) {
-        int result = 1;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
-        }
-        return result;
     }
 }
