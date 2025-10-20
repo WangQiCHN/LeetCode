d5446fc465d77d5d172b9bb38670d6b144f927c0
王骐 <wangqichn@gmail.com>
2025-10-20 20:18:12 +0800

    refine 3003

--- code/Solution.java
+++ code/Solution.java
@@ -1,91 +1,68 @@
 package code;
 
-class Solution {
+public class Solution {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        String s = "84688926";
-        int a = 2;
-        int b = 5;
-        String res = solution.findLexSmallestString(s, a, b);
-        System.out.println(res);
+        Solution sol = new Solution();
+        String s = "abbba";
+        int k = 2;
+        int result = sol.maxPartitionsAfterOperations(s, k);
+        System.out.println("Max partitions: " + result);
     }
+    public int maxPartitionsAfterOperations(String s, int k) {
+        int n = s.length();
+        int[][] left = new int[n][3];
+        int[][] right = new int[n][3];
 
-    public String findLexSmallestString(String s, int a, int b) {
-        int len = s.length();
-        int rotate = len / gcd(len, b);
-        int[] nums = getArray(s, len);
-        int[] min = getArray(s, len);
-        for (int i = 0; i < rotate; i++) {
-            int[] newNums = rotateN(nums, b * i, len);
-            addN(newNums, a, len, 1);
-            if (b % 2 == 1) {
-                addN(newNums, a, len, 0);
+        int num = 0, mask = 0, count = 0;
+        for (int i = 0; i < n - 1; i++) {
+            int binary = 1 << (s.charAt(i) - 'a');
+            if ((mask & binary) == 0) {
+                count++;
+                if (count <= k) {
+                    mask |= binary;
+                } else {
+                    num++;
+                    mask = binary;
+                    count = 1;
+                }
             }
-            if (isSmaller(newNums, min, len)) {
-                min = newNums;
+            left[i + 1][0] = num;
+            left[i + 1][1] = mask;
+            left[i + 1][2] = count;
+        }
+
+        num = 0;
+        mask = 0;
+        count = 0;
+        for (int i = n - 1; i > 0; i--) {
+            int binary = 1 << (s.charAt(i) - 'a');
+            if ((mask & binary) == 0) {
+                count++;
+                if (count <= k) {
+                    mask |= binary;
+                } else {
+                    num++;
+                    mask = binary;
+                    count = 1;
+                }
             }
+            right[i - 1][0] = num;
+            right[i - 1][1] = mask;
+            right[i - 1][2] = count;
         }
 
-        return generateS(min);
-    }
-
-    private int gcd(int a, int b) {
-        if (b == 0) {
-            return a;
-        }
-        int tmp = b;
-        b = a % b;
-        return gcd(tmp, b);
-    }
-
-    private int[] getArray(String s, int n) {
-        int[] nums = new int[n];
+        int maxVal = 0;
         for (int i = 0; i < n; i++) {
-            nums[i] = (int) (s.charAt(i) - '0');
-        }
-        return nums;
-    }
-
-    private int[] rotateN(int[] nums, int step, int n) {
-        int[] result = new int[n];
-        for (int i = 0; i < n; i++) {
-            result[(i + step) % n] = nums[i];
-        }
-
-        return result;
-    }
-
-    private void addN(int[] nums, int N, int n, int start) {
-        int times = 0, min = 9;
-        for (int i = 0; i <= 9; i++) {
-            int tmp = (nums[start] + N * i) % 10;
-            if (tmp < min) {
-                min = tmp;
-                times = i;
+            int seg = left[i][0] + right[i][0] + 2;
+            int totMask = left[i][1] | right[i][1];
+            int totCount = Integer.bitCount(totMask);
+            if (left[i][2] == k && right[i][2] == k && totCount < 26) {
+                seg++;
+            } else if (Math.min(totCount + 1, 26) <= k) {
+                seg--;
             }
+            maxVal = Math.max(maxVal, seg);
         }
-
-        for (int i = start; i < n; i += 2) {
-            nums[i] = (nums[i] + N * times) % 10;
-        }
-    }
-
-    private boolean isSmaller(int[] nums1, int[] nums2, int n) {
-        for (int i = 0; i < n; i++) {
-            if (nums1[i] < nums2[i])
-                return true;
-            else if (nums1[i] > nums2[i])
-                return false;
-        }
-        return false;
-    }
-
-    private String generateS(int[] nums) {
-        StringBuilder sb = new StringBuilder();
-        for (int n : nums) {
-            sb.append(n);
-        }
-
-        return sb.toString();
+        return maxVal;
     }
 }
