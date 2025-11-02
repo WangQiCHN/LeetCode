35cca708e8384bff0dfb3e853b4974b084cfdd5d
王骐 <wangqichn@gmail.com>
2025-11-02 21:55:20 +0800

    go to 350

--- code/Solution.java
+++ code/Solution.java
@@ -1,13 +1,16 @@
 package code;
 
+import java.util.Map;
+import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
-        // String[] words = { "abcd", "dcba", "lls", "s", "sssll" };
-        String[] words = { "aa", "abc", "ba", "cba", "baa", "" };
+        String[] words = { "abcd", "dcba", "lls", "s", "sssll" };
+        // String[] words = { "a", "b", "c", "ab", "ac", "aa" };
         List<List<Integer>> result = s.palindromePairs(words);
         for (List<Integer> item : result) {
             System.out.println("{" + item.get(0) + "," + item.get(1) + "}");
@@ -15,130 +18,61 @@ public class Solution {
     }
 
     public List<List<Integer>> palindromePairs(String[] words) {
+        Map<String, Integer> dict = new HashMap<>();
+        List<List<Integer>> result = new ArrayList<>();
         int n = words.length;
-        boolean[][] pref = new boolean[n][];
-        boolean[][] suf = new boolean[n][];
-
         for (int i = 0; i < n; i++) {
-            String s = words[i];
-            // 这个函数的作用是获得在第N位是不是一个回文串
-            boolean[] p = prefixPalFlags(s);
-            String rs = new StringBuilder(s).reverse().toString();
-            boolean[] pr = prefixPalFlags(rs);
-            int m = s.length();
-            // 反转顺序的内容
-            boolean[] sf = new boolean[m + 1];
-            for (int ppos = 0; ppos <= m; ppos++) {
-                sf[ppos] = pr[m - ppos];
-            }
-            pref[i] = p;
-            suf[i] = sf;
+            dict.put(words[i], i);
         }
 
-        Node root = new Node();
         for (int i = 0; i < n; i++) {
-            String w = words[i];
-            int m = w.length();
-            Node u = root;
-            for (int d = 0; d < m; d++) {
-                int L = m - d;
-                if (pref[i][L])
-                    u.pal.add(i);
-                char c = w.charAt(m - 1 - d);
-                int idx = c - 'a';
-                if (u.ch[idx] == null)
-                    u.ch[idx] = new Node();
-                u = u.ch[idx];
-            }
-            u.pal.add(i);
-            u.wordIndex = i;
-        }
-
-        List<List<Integer>> ans = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            String w = words[i];
-            int m = w.length();
-            Node u = root;
-            boolean broken = false;
-            for (int p = 0; p < m; p++) {
-                if (u.wordIndex != -1 && u.wordIndex != i && suf[i][p]) {
-                    ArrayList<Integer> pair = new ArrayList<>(2);
-                    pair.add(i);
-                    pair.add(u.wordIndex);
-                    ans.add(pair);
+            String word = words[i];
+            String rword = new StringBuilder().append(word).reverse().toString();
+            for (int j = 0; j <= word.length(); j++) {
+                String a = word.substring(0, j);
+                String b = word.substring(j, word.length());
+                String ra = rword.substring(rword.length() - j, rword.length());
+                String rb = rword.substring(0, rword.length() - j);
+                if (isPalindrome(a) && dict.containsKey(rb)) {
+                    int index = dict.get(rb);
+                    if (index != i) {
+                        if (word.length() == rb.length()) {
+                            if (index > i) {
+                                result.add(Arrays.asList(index, i));
+                            }
+                        } else {
+                            result.add(Arrays.asList(index, i));
+                        }
+                    }
                 }
-                int idx = w.charAt(p) - 'a';
-                if (u.ch[idx] == null) {
-                    broken = true;
-                    break;
-                }
-                u = u.ch[idx];
-            }
-            if (!broken) {
-                for (int j : u.pal) {
-                    if (j != i) {
-                        ArrayList<Integer> pair = new ArrayList<>(2);
-                        pair.add(i);
-                        pair.add(j);
-                        ans.add(pair);
+                if (isPalindrome(b) && dict.containsKey(ra)) {
+                    int index = dict.get(ra);
+                    if (index != i) {
+                        if (word.length() == ra.length()) {
+                            if (index > i) {
+                                result.add(Arrays.asList(i, index));
+                            }
+                        } else {
+                            result.add(Arrays.asList(i, index));
+                        }
                     }
                 }
             }
         }
 
-        return ans;
+        return result;
     }
 
-    private boolean[] prefixPalFlags(String s) {
-        int n = s.length();
-        boolean[] pref = new boolean[n + 1];
-        pref[0] = true;
-        if (n == 0)
-            return pref;
-
-        int[] d1 = new int[n];
-        int l = 0, r = -1;
-        for (int i = 0; i < n; i++) {
-            int k = (i > r) ? 1 : Math.min(d1[l + r - i], r - i + 1);
-            while (i - k >= 0 && i + k < n && s.charAt(i - k) == s.charAt(i + k))
-                k++;
-            d1[i] = k;
-            if (i + k - 1 > r) {
-                l = i - k + 1;
-                r = i + k - 1;
-            }
+    private boolean isPalindrome(String s) {
+        int len = s.length();
+        int l = 0, r = len - 1;
+        while (l <= r) {
+            if (s.charAt(l) != s.charAt(r))
+                return false;
+            l++;
+            r--;
         }
 
-        int[] d2 = new int[n];
-        l = 0;
-        r = -1;
-        for (int i = 0; i < n; i++) {
-            int k = (i > r) ? 0 : Math.min(d2[l + r - i + 1], r - i + 1);
-            while (i - k - 1 >= 0 && i + k < n && s.charAt(i - k - 1) == s.charAt(i + k))
-                k++;
-            d2[i] = k;
-            if (i + k - 1 > r) {
-                l = i - k;
-                r = i + k - 1;
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            if (d1[i] >= i + 1) {
-                int L = 2 * i + 1;
-                pref[L] = true;
-            }
-            if (d2[i] >= i) {
-                int L = 2 * i;
-                pref[L] = true;
-            }
-        }
-        return pref;
+        return true;
     }
-}
-
-class Node {
-    Node[] ch = new Node[3]; // 这个节点下面的子节点
-    int wordIndex = -1; // 在这个节点，第一个单词结束了
-    ArrayList<Integer> pal = new ArrayList<>(); // 是和这个词符合的一个顺序
 }
