d4b435a088bd25ff5927e42e4bc6c8904d2467e9
王骐 <wangqichn@gmail.com>
2025-11-08 14:20:41 +0800

    understand 2528

--- code/Solution.java
+++ code/Solution.java
@@ -1,105 +1,65 @@
 package code;
 
-import java.util.*;
+import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int[] nums = {3,8,7,8,7,5};
+        int[] stations = { 1, 2, 4, 5, 0 };
+        // int[] stations = {13,12,8,14,7};
+        int r = 1;
         int k = 2;
-        int x = 2;
-        long[] result = sol.findXSum(nums, k, x);
-        for (long r : result) {
-            System.out.println(r);
-        }
+        long result = sol.maxPower(stations, r, k);
+        System.out.println(result);
     }
-    private static final long OCC_KEY = 1000000001L;
-    public long[] findXSum(int[] nums, int k, int x) {
-        Map<Integer, Integer> occurence = new HashMap<>();
-        TreeSet<Long> min = new TreeSet<>();
-        TreeSet<Long> max = new TreeSet<>((a, b) -> b.compareTo(a));
-        long total = 0;
 
-        List<Long> result = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
-            if (!occurence.containsKey(num)) {
-                occurence.put(num, 1);
-                long key = OCC_KEY + num;
-                if (min.size() < x) {
-                    min.add(key);
-                    total += ((long)num);
-                } else {
-                    max.add(key);
-                }
+
+    public long maxPower(int[] stations, int r, int k) {
+        int n = stations.length;
+        long[] cnt = new long[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            int left = Math.max(0, i - r);
+            int right = Math.min(n, i + r + 1);
+            cnt[left] += stations[i];
+            cnt[right] -= stations[i];
+        }
+
+        long lo = Arrays.stream(stations).min().getAsInt();
+        long hi = Arrays.stream(stations).asLongStream().sum() + k;
+        long res = 0;
+
+        while (lo <= hi) {
+            long mid = lo + (hi - lo) / 2;
+            if (check(cnt, mid, r, k)) {
+                res = mid;
+                lo = mid + 1;
             } else {
-                int cnt = occurence.get(num);
-                occurence.put(num, cnt + 1);
-                long key = OCC_KEY * cnt + num;
-                if (min.contains(key)) {
-                    min.remove(key);
-                    min.add(key + OCC_KEY);
-                    total += ((long)num);
-                } else {
-                    max.remove(key);
-                    max.add(key + OCC_KEY);
-                }
-            }
-
-            if (i >= k) {
-                num = nums[i - k];
-                int cnt = occurence.get(num);
-                long key = OCC_KEY * cnt + num;
-                if (cnt == 1) {
-                    occurence.remove(num);
-                } else {
-                    occurence.put(num, cnt - 1);
-                }
-                if (min.contains(key)) {
-                    min.remove(key);
-                    if (cnt != 1) {
-                        min.add(key - OCC_KEY);
-                    }
-                    total -= num;
-                } else {
-                    max.remove(key);
-                    if (cnt != 1) {
-                        max.add(key - OCC_KEY);
-                    }
-                }
-            }
-
-            if (min.size() == x - 1 && max.size() > 0) {
-                long key = max.first();
-                min.add(key);
-                max.remove(key);
-                total += (key % OCC_KEY) * (key / OCC_KEY);
-            } else if (min.size() == x && max.size() > 0) {
-                long maxKey = max.first();
-                long minKey = min.first();
-                if (maxKey > minKey) {
-                    total -= (minKey % OCC_KEY) * (minKey / OCC_KEY);
-                    total += (maxKey % OCC_KEY) * (maxKey / OCC_KEY);
-                    max.remove(maxKey);
-                    min.remove(minKey);
-                    max.add(minKey);
-                    min.add(maxKey);
-                }
-            }
-            if (i + 1 >= k) {
-                result.add(total);
+                hi = mid - 1;
             }
         }
-
-        return toLong(result);
+        return res;
     }
 
-    private long[] toLong(List<Long> result) {
-        long[] answer = new long[result.size()];
-        for (int i = 0; i < result.size(); i++) {
-            answer[i] = result.get(i);
-        }
+    private boolean check(long[] cnt, long val, int r, int k) {
+        int n = cnt.length - 1;
+        long[] diff = cnt.clone();
+        long sum = 0;
+        long remaining = k;
 
-        return answer;
+        for (int i = 0; i < n; i++) {
+            sum += diff[i];
+            if (sum < val) {
+                long add = val - sum;
+                if (remaining < add) {
+                    return false;
+                }
+                remaining -= add;
+                int end = Math.min(n, i + 2 * r + 1);
+                diff[end] -= add;
+                sum += add;
+            }
+        }
+        return true;
     }
 }
