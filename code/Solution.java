c4b59e84b65e66040ffd4b3a850817f1aa173e1a
王骐 <wangqichn@gmail.com>
2025-11-16 19:48:12 +0800

    finish daily test

--- code/Solution.java
+++ code/Solution.java
@@ -3,24 +3,40 @@ package code;
 public class Solution {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        String s = "00011";
+        String s = "101101";
+        // String s = "00011";
+        // String s = "1";
         int result = sol.numberOfSubstrings(s);
         System.out.println(result);
     }
     public int numberOfSubstrings(String s) {
-        int len = s.length();
-        int total = 0;
-        for (int i = 0; i < len; i++) {
-            int one = 0, zero = 0;
-            for (int j = i; j < len - 1; j++) {
-                char c = s.charAt(j);
-                if (c == '0') zero++;
-                else one++;
-                if (one >= (zero * zero)) {
-                    total++;
-                }
+        int n = s.length();
+        int[] preZero = new int[n + 1];
+        preZero[0] = -1;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '0') {
+                preZero[i + 1] = i;
+            } else {
+                preZero[i + 1] = preZero[i];
             }
         }
-        return total;
+        
+
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            int cnt0 = (s.charAt(i) == '0' ? 1 : 0);
+            int j = i;
+            int mul = cnt0 * cnt0;
+            while (j != -1 && mul <= i) {
+                int cnt1 = i - preZero[j] - cnt0;
+                if (mul <= cnt1) {
+                    res += Math.min(j - preZero[j], cnt1 - mul + 1);
+                }
+                j = preZero[j];
+                cnt0++;
+                mul = cnt0 * cnt0;
+            }
+        }
+        return res;
     }
 }
