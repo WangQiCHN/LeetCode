2abe2a34362fb7611095e3430ad96dad4d5590a1
王骐 <wangqichn@gmail.com>
2025-11-22 18:35:06 +0800

    finish 399 test

--- code/Solution.java
+++ code/Solution.java
@@ -1,51 +1,116 @@
 package code;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Solution {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        String s = "aaaaaaaaabbbcccccddddd";
-        int k = 5;
-        int result = sol.longestSubstring(s, k);
+        List<List<String>> equations = new ArrayList<>();
+        equations.add(Arrays.asList("dd", "ff"));
+        equations.add(Arrays.asList("aa", "dd"));
+        equations.add(Arrays.asList("a", "aa"));
+        // equations.add(Arrays.asList("bc", "cd"));
+        // double[] values = { 1.5, 2.5, 5.0 };
+        double[] values = { 3.0, 2.0, 2.0 };
+        List<List<String>> queries = new ArrayList<>();
+        // [["a","c"],["c","b"],["bc","cd"],["cd","bc"]]
+        queries.add(Arrays.asList("ff", "a"));
+        // queries.add(Arrays.asList("bc", "cd"));
+        // queries.add(Arrays.asList("cd", "bc"));
+        double[] result = sol.calcEquation(equations, values, queries);
         System.out.println(result);
     }
-    public int longestSubstring(String s, int k) {
-        if (k > s.length()) return 0;
-        int[] win = new int[26];
-        int n = s.length();
-        for (char c : s.toCharArray()) {
-            win[c - 'a']++;
-        }
 
-        if (isValid(win, k) == 1) return n;
-
-        int l = 0, r = 0;
-        int result = 0;
-        while (r < n) {
-            int index = s.charAt(r) - 'a';
-            r++;
-            if (win[index] >= k) {
-                continue;
-            } else {
-                result = Math.max(result, longestSubstring(s.substring(l, r - 1), k));
-                l = r;
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        int sz = equations.size();
+        Union uf = new Union(sz * 2);
+        Map<String, Integer> dict = new HashMap<>();
+        int id = 0;
+        for (int i = 0; i < sz; i++) {
+            List<String> equation = equations.get(i);
+            double v = values[i];
+            String p = equation.get(0);
+            String q = equation.get(1);
+            if (!dict.containsKey(p)) {
+                dict.put(p, id);
+                id++;
             }
+            if (!dict.containsKey(q)) {
+                dict.put(q, id);
+                id++;
+            }
+            int pi = dict.get(p);
+            int qi = dict.get(q);
+            uf.connect(pi, qi, v);
         }
-        result = Math.max(result, longestSubstring(s.substring(l, r), k));
+
+        double[] result = new double[queries.size()];
+        int index = 0;
+        for (List<String> query : queries) {
+            String p = query.get(0);
+            String q = query.get(1);
+            int pi = -1;
+            int qi = -1;
+            if (dict.containsKey(p)) {
+                pi = dict.get(p);
+            }
+            if (dict.containsKey(q)) {
+                qi = dict.get(q);
+            }
+            if (pi == -1 || qi == -1) {
+                result[index] = -1.0;
+            } else {
+                result[index] = uf.isConnect(pi, qi);
+            }
+            index++;
+        }
 
         return result;
     }
+}
 
-    // -1 means useless, 0 means analyze it, 1 means satisfy
-    private int isValid(int[] win, int k) {
-        boolean hasValid = false, hasProblem = false;
-        for (int i = 0; i < 26; i++) {
-            if (win[i] == 0) continue;
-            else if (win[i] >= k) hasValid = true;
-            else hasProblem = true;
+class Union {
+    int[] parents;
+    double[] weights;
+
+    public Union(int n) {
+        parents = new int[n];
+        weights = new double[n];
+        Arrays.fill(weights, 1.0d);
+        for (int i = 0; i < n; i++) {
+            parents[i] = i;
+        }
+    }
+
+    public void connect(int p, int q, double v) {
+        int pp = find(p);
+        int qp = find(q);
+        if (pp != qp) {
+            parents[pp] = qp;
+            weights[pp] = weights[q] * v / weights[p];
+        }
+    }
+
+    public double isConnect(int p, int q) {
+        int pp = find(p);
+        int qp = find(q);
+        if (pp != qp) {
+            return -1.0;
+        }
+        else return weights[p] / weights[q];
+    }
+
+    private int find(int x) {
+        if (x != parents[x]) {
+            int origin = parents[x];
+            parents[x] = find(origin);
+            weights[x] *= weights[origin];
         }
 
-        if (hasValid && !hasProblem) return 1;
-        else if (hasValid && hasProblem) return 0;
-        else return -1;
+        return parents[x];
     }
 }
