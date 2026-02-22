87bfaff24da040164b843000a168e28ac672ad8c
王骐 <wangqichn@gmail.com>
2026-02-22 20:38:19 +0800

    first round of 440

--- code/my/Solution.java
+++ code/my/Solution.java
@@ -3,11 +3,38 @@ package code.my;
 class Solution {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int n = 13, k = 6;
+        int n = 13, k = 2;
         int ans = sol.findKthNumber(n, k);
         System.out.println(ans);
     }
     public int findKthNumber(int n, int k) {
-        return 0;
+        int current = 1;
+        k--;
+        
+        while (k > 0) {
+            int steps = getSteps(current, n);
+            if (steps <= k) {
+                k -= steps;
+                current++;
+            } else {
+                current = current * 10;
+                k--;
+            }
+        }
+
+        return current;
+    }
+    private int getSteps(int cnt, int n) {
+        long first = cnt;
+        long last = cnt;
+        int steps = 0;
+
+        while (first <= n) {
+            steps += Math.min(last, n) - first + 1;
+            first *= 10;
+            last = last * 10 + 9;
+        }
+
+        return steps;
     }
 }
