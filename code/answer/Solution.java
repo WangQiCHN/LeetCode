528a7263ed05e1011c46dbe5604bcca2f6415c92
王骐 <wangqichn@gmail.com>
2026-03-01 18:50:34 +0800

    target: go to 500

--- code/answer/Solution.java
+++ code/answer/Solution.java
@@ -1,5 +1,6 @@
 package code.answer;
 
+/*
 class Solution {
     public static void main(String[] args) {
         Solution sol = new Solution();
@@ -33,5 +34,69 @@ class Solution {
             last = last * 10 + 9;
         }
         return steps;
+    }
+}
+    */
+import java.util.*;
+class Solution {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int maxChoosableInteger = 18;
+        int desiredTotal = 300;
+        boolean ans = sol.canIWin(maxChoosableInteger, desiredTotal);
+        System.out.println(ans);
+    }
+    private boolean[] visited;
+    private Map<String, Boolean> memo = new HashMap<>();
+    public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
+        int total = (maxChoosableInteger + 1) * maxChoosableInteger / 2;
+        if (total < desiredTotal) return false;
+
+        visited = new boolean[maxChoosableInteger];
+
+        return dfs(maxChoosableInteger, desiredTotal, 0);
+    }
+
+    private boolean dfs(int maxChoosableInteger, int desiredTotal, int currentTotal) {
+        String key = getKey();
+        boolean res = false;
+        if (!memo.containsKey(key)) {
+            for (int i = 0; i < maxChoosableInteger; i++) {
+                if (!visited[i] && i + 1 + currentTotal >= desiredTotal) {
+                    res = true;
+                    break;
+                }
+            }
+            if (!res) {
+                for (int i = 0; i < maxChoosableInteger; i++) {
+                    if (visited[i]) continue;
+                    else {
+                        visited[i] = true;
+                        if (!dfs(maxChoosableInteger, desiredTotal, currentTotal + i + 1)) {
+                            res = true;
+                        }
+                        visited[i] = false;
+                        if (res) {
+                            break;
+                        }
+                    }
+                }
+            }
+            memo.put(key, res);
+        }
+        return memo.get(key);
+    }
+
+    private String getKey() {
+        StringBuilder sb = new StringBuilder();
+        for (boolean v : visited) {
+            if (v) {
+                sb.append(1);
+            } else {
+                sb.append(0);
+            }
+        }
+
+        return sb.toString();
     }
 }
