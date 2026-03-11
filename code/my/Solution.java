7e902c7173b5ae926b650b603e160179a063bc98
王骐 <wangqichn@gmail.com>
2026-03-12 00:21:31 +0800

    finish 490

--- code/my/Solution.java
+++ code/my/Solution.java
@@ -1,71 +1,56 @@
 package code.my;
 
-import java.util.Map;
-import java.util.HashMap;
+import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-        String s1 = "aaa";
-        String s2 = "aaaaa";
-        int n1 = 20;
-        int n2 = 1;
-        System.out.println(new Solution().getMaxRepetitions(s1, n1, s2, n2));
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 1, 1, 1, 1 };
+        System.out.println(new Solution().findSubsequences(nums));
     }
 
-    public int getMaxRepetitions(String s1, int n1, String s2, int n2) {
-        Map<Integer, Record> dict = new HashMap<>();
-        int s1Cnt = 0, s2Cnt = 0;
-        int j = 0;
-        Record cnt, prev;
-        while (true) {
-            s1Cnt++;
-            for (int i = 0; i < s1.length(); i++) {
-                char c = s1.charAt(i);
-                if (c == s2.charAt(j)) {
-                    j++;
-                    if (j == s2.length()) {
-                        j = 0;
-                        s2Cnt++;
-                    }
-                }
-            }
-            if (s1Cnt == n1) {
-                return s2Cnt / n2;
-            }
+    List<List<Integer>> result = new ArrayList<>();
+    Set<String> set = new HashSet<>();
 
-            if (dict.containsKey(j)) {
-                prev = dict.get(j);
-                cnt = new Record(s1Cnt - prev.s1Cnt, s2Cnt - prev.s2Cnt);
-                break;
-            } else {
-                dict.put(j, new Record(s1Cnt, s2Cnt));
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        LinkedList<Integer> list = new LinkedList<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                list.add(nums[i]);
+                bfs(nums, i + 1, list);
+                list.removeLast();
             }
         }
-        int answer = prev.s2Cnt + (n1 - prev.s1Cnt) / cnt.s1Cnt * cnt.s2Cnt;
-        int rest = (n1 - prev.s1Cnt) % cnt.s1Cnt;
-        int k = j;
-        for (int i = 0; i < rest; i++) {
-            for (j = 0; j < s1.length(); j++) {
-                char c = s1.charAt(j);
-                if (c == s2.charAt(k)) {
-                    k++;
-                    if (k == s2.length()) {
-                        k = 0;
-                        answer++;
-                    }
-                }
-            }
-        }
-        return answer / n2;
+        return result;
     }
-}
 
-class Record {
-    int s1Cnt;
-    int s2Cnt;
+    private void bfs(int[] nums, int start, LinkedList<Integer> list) {
+        if (list.size() >= 2) {
+            ArrayList<Integer> arrayList = new ArrayList<>();
+            arrayList.addAll(list);
+            String key = getKey(arrayList);
+            if (!set.contains(key)) {
+                result.add(arrayList);
+                set.add(key);
+            }
+        }
+        int n = nums.length;
+        for (int i = start; i < n; i++) {
+            if (nums[i] >= list.peekLast() && (i == start || nums[i] != nums[i - 1])) {
+                list.add(nums[i]);
+                bfs(nums, i + 1, list);
+                list.removeLast();
+            }
+        }
+    }
 
-    public Record(int s1Cnt, int s2Cnt) {
-        this.s1Cnt = s1Cnt;
-        this.s2Cnt = s2Cnt;
+    private String getKey(ArrayList<Integer> arrayList) {
+        StringBuffer sb = new StringBuffer();
+        for (int v : arrayList) {
+            sb.append(v);
+            sb.append('#');
+        }
+
+        return sb.toString();
     }
 }
