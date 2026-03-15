d4bdf9b634f52e2d9d69d9e3cd32e092459fd921
王骐 <wangqichn@gmail.com>
2026-03-15 11:05:37 +0800

    finish it

--- code/my/Solution.java
+++ code/my/Solution.java
@@ -5,6 +5,7 @@ import java.util.*;
 class Solution {
   public static void main(String[] args) {
     int[] nums = { 0, 4, 6, 0, 3, 2, 6, 9, 4, 1 };
+    // int[] nums = {1,2,3,4,2,3,1,4,2};
     int k = 3;
     double[] result = new Solution().medianSlidingWindow(nums, k);
     System.out.println(result);
@@ -15,6 +16,7 @@ class Solution {
     PriorityQueue<Integer> min = new PriorityQueue<>((a, b) -> b.compareTo(a)); // 返回最大的
     double[] result = new double[nums.length - k + 1];
     int[] sizes = new int[2]; // maxSize - sizes[0], minSize - sizes[1]
+    Map<Integer, Integer> delayed = new HashMap<>();
     for (int i = 0; i < nums.length; i++) {
       int num = nums[i];
       if (sizes[0] == 0 || num >= max.peek()) {
@@ -25,15 +27,7 @@ class Solution {
         min.offer(num);
       }
       // make balance
-      if (sizes[0] - sizes[1] == 2) {
-        min.offer(max.poll());
-        sizes[1]++;
-        sizes[0]--;
-      } else if (sizes[0] - sizes[1] == -1) {
-        max.offer(min.poll());
-        sizes[0]++;
-        sizes[1]--;
-      }
+      makeBalance(sizes, max, min, delayed);
 
       if (i + 1 >= k) {
         if (k % 2 == 1) {
@@ -43,26 +37,51 @@ class Solution {
         }
         // remove one element
         int removed = nums[i - k + 1];
-        if (min.contains(removed)) {
-          min.remove(removed);
-          sizes[1]--;
-        } else if (max.contains(removed)) {
-          max.remove(removed);
+        delayed.put(removed, delayed.getOrDefault(removed, 0) + 1);
+        if (removed >= max.peek()) {
           sizes[0]--;
-        }
-        // make balance
-        if (sizes[0] - sizes[1] == 2) {
-          min.offer(max.poll());
-          sizes[1]++;
-          sizes[0]--;
-        } else if (sizes[0] - sizes[1] == -1) {
-          max.offer(min.poll());
-          sizes[0]++;
+          if (removed == max.peek()) {
+            prune(max, delayed);
+          }
+        } else {
           sizes[1]--;
+          if (removed == min.peek()) {
+            prune(min, delayed);
+          }
         }
+        makeBalance(sizes, max, min, delayed);
       }
     }
 
     return result;
+  }
+
+  private void makeBalance(int[] sizes, PriorityQueue<Integer> max, PriorityQueue<Integer> min, Map<Integer, Integer> delayed) {
+    if (sizes[0] - sizes[1] == 2) {
+      min.offer(max.poll());
+      sizes[1]++;
+      sizes[0]--;
+      prune(max, delayed);
+    } else if (sizes[0] - sizes[1] == -1) {
+      max.offer(min.poll());
+      sizes[0]++;
+      sizes[1]--;
+      prune(min, delayed);
+    }
+  }
+
+  private void prune(PriorityQueue<Integer> queue, Map<Integer, Integer> delayed) {
+    while (!queue.isEmpty()) {
+      int v = queue.peek();
+      if (delayed.containsKey(v)) {
+        delayed.put(v, delayed.get(v) - 1);
+        if (delayed.get(v) == 0) {
+          delayed.remove(v);
+        }
+        queue.poll();
+      } else {
+        break;
+      }
+    }
   }
 }
