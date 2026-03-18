5385d7e15f3591695b642e5446249336c12bdaa7
王骐 <wangqichn@gmail.com>
2026-03-18 21:57:06 +0800

    add basic code of merge sort

--- code/my/Solution.java
+++ code/my/Solution.java
@@ -2,29 +2,53 @@ package code.my;
 
 class Solution {
   public static void main(String[] args) {
-    String n = "4681";
-    System.out.println(new Solution().smallestGoodBase(n));
+    int[] nums = { 2, 4, 3, 5, 1 };
+    System.out.println(new Solution().reversePairs(nums));
   }
 
-  public String smallestGoodBase(String n) {
-    long ln = Long.parseLong(n);
-    int upper = (int) (Math.log(ln) / Math.log(2));
-    for (int m = upper; m >= 2; m--) {
-      int k = (int) Math.pow(ln, 1.0 / m);
-      long mul = 1, sum = 1;
-      for (int i = 1; i <= m; i++) {
-        mul *= k;
-        sum += mul;
-      }
+  public int reversePairs(int[] nums) {
+    // it is a merge sort
+    if (nums.length == 0) {
+      return 0;
+    }
+    int result = reverseMergeSort(nums, 0, nums.length - 1);
+    return result;
+  }
 
-      if (sum == ln) {
-        return k + "";
+  private int reverseMergeSort(int[] nums, int s, int e) {
+    if (s == e)
+      return 0;
+    int m = (s + e) / 2;
+    int n1 = reverseMergeSort(nums, s, m);
+    int n2 = reverseMergeSort(nums, m + 1, e);
+
+    int[] sorted = new int[e - s + 1];
+    int l = s, r = m + 1, i = 0;
+    while (l <= m && r <= e) {
+      if (nums[l] < nums[r]) {
+        sorted[i] = nums[l];
+        l++;
+      } else {
+        sorted[i] = nums[r];
+        r++;
+      }
+      i++;
+    }
+    if (l > m) {
+      while (r <= e) {
+        sorted[i++] = nums[r++];
+      }
+    }
+    if (r > e) {
+      while (l <= m) {
+        sorted[i++] = nums[l++];
       }
     }
 
-    return ln - 1 + "";
+    for (i = s; i <= e; i++) {
+      nums[i] = sorted[i - s];
+    }
+
+    return n1 + n2;
   }
 }
-
-// 4681 - 8^4 + 8^3 + 8^2 + 8 + 1
-//
